1570bab8262a004376662d233683b6863bbf7b45
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-09-30 20:28:50 +0100

    Add new process add Split content from process

--- apps/process-app/src/app/app.module.ts
+++ apps/process-app/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
-import { ContentLibModule } from '@my-company/content-lib';
+import { ProcessLibModule } from '@my-company/process-lib';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot(), ContentLibModule],
+  imports: [BrowserModule, NxModule.forRoot(), ProcessLibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
